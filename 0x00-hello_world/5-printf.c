4ec3ffd8d0ae96331bf9237f3a7d135bf7dd54af
Dukurma <barryyabilsu@gmail.com>
2022-12-08 14:26:20 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,5 +7,5 @@
  int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+		return (0);
 }
