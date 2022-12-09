401499e2b64fd8273fe414ea579b3ea0a3c532bc
Dukurma <barryyabilsu@gmail.com>
2022-12-09 11:56:57 -0800

    new coreection

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main -Entry point
  *
@@ -11,8 +12,10 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
