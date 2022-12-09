4619e8e2aa460e42f6dc6293d7f28c603ad3ef22
Dukurma <barryyabilsu@gmail.com>
2022-12-08 18:22:22 -0800

    quote101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", sign);
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
