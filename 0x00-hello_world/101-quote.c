9af424f45d5a05d3920474f845883f4117e11b8e
Dukurma <barryyabilsu@gmail.com>
2022-12-08 17:44:26 -0800

    quote 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" -Dora korpa, 2015-10-19\n", 60);
+	return (1);
+}
