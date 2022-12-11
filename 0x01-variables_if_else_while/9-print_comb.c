dffc57a32af83d015b608587a620e73e50d63d51
Dukurma <barryyabilsu@gmail.com>
2022-12-11 01:43:19 -0800

    all combos

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		putchar(b);
+		if (b != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
