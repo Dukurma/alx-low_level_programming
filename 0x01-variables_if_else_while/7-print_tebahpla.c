9fc23b162ae0ce2369826b1916246b136c076fa6
Dukurma <barryyabilsu@gmail.com>
2022-12-10 23:28:02 -0800

    reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
 		putchar('\n');
 	return (0);
 }
