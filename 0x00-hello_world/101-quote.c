278cef76986bd73508ebd568ebdef2b2229f88ee
Dukurma <barryyabilsu@gmail.com>
2022-12-12 00:04:57 -0800

    DORA

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
