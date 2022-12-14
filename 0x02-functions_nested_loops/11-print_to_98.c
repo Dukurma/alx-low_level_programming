9a393f742940ce0a3bb793554f5e1f05feee885f
Dukurma <barryyabilsu@gmail.com>
2022-12-13 17:14:53 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,16 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d", n--);
+			printf("%d ", n--);
 		printf("%d\n", n);
-		{
-			printf(", ");
-		}
+		printf(",");
 	}
 	else
 	{
 		while (n < 98)
-			printf("%d", n++);
+			printf("%d ", n++);
 		printf("%d\n", n);
-		{
-			printf(", ");
+		printf(",");
 		}
-	}
 }
 
