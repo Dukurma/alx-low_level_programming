4d8436e989700a356cc19911861c730d5c985aec
Dukurma <barryyabilsu@gmail.com>
2022-12-13 17:31:04 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,33 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n < 98)
 	{
-		while (n > 98)
-			printf("%d ", n--);
-		printf("%d\n", n);
-		printf(",");
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
 	}
 	else
 	{
-		while (n < 98)
-			printf("%d ", n++);
-		printf("%d\n", n);
-		printf(",");
-		}
+		printf("98");
+	}
+	printf("\n");
 }
-
