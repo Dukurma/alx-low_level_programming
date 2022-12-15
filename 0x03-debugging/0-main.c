40f771861e9ab8911c56989c1ea58386608ba14d
Dukurma <barryyabilsu@gmail.com>
2022-12-15 07:15:42 -0800

    multiple main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,15 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * 
- * Rerurn: 0 (success)
- */
+ * Return: 0
+ * /
 
 int main(void)
 {
-	int i;
+       	int i;
 
-	i = 0;
+	i =0;       
 	positive_or_negative(i);
-
+	
 	return (0);
 }
