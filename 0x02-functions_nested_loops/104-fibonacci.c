b1f24f0153827a1df71b9298153e7ba9f4d05af2
Dukurma <barryyabilsu@gmail.com>
2022-12-16 04:41:15 -0800

    fibo 4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,35 +21,41 @@ int numLength(int num)
 }
 /**
  * *main - prints the first 98 fibonaci sequences
+ * Solution was copied from Nobert Patrick
+ * Wise, github handle: Trikcode
  * Return: 0
  */
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
-	short int i = 1, initial0s;
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	while (i <= 98)
+	for (count = 1; count <= 98; ++count)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
 		initial0s = numLength(mx) - 1 - numLength(f1);
+
 		while (f1o > 0 && initial0s > 0)
 		{
-			printf("%i", 0);
-			initial0s--;
+			printf("%d", 0);
+			--initial0s;
 		}
+
 		printf("%lu", f1);
-		tmp = (f1 + f2) % mx;
-		tmpo = f1o + f2o + (f1 + f2) / mx;
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
 		f1 = f2;
 		f1o = f2o;
-		f2 = tmp;
-		f2o = tmpo;
+		f2 = sum;
+		f2o = sumo;
 
-		if (i != 98)
+		if (count != 98)
 			printf(", ");
-		i++;
+		else
+			printf("\n");
 	}
+
 	return (0);
 }
-
