61fa3428b748d7d136260fff90e658554f56a1b6
Dukurma <barryyabilsu@gmail.com>
2022-12-19 11:50:57 -0800

    stray cat

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,15 +5,16 @@
  * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
- * Return: string
+ * Return: Pointer to `dest`
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i = -1;
 
-	for (; src[i] != '\0'; i++)
-	{
+	do {
+		i++;
 		dest[i] = src[i];
-	}
+	} while (src[i] != '\0');
+
 	return (dest);
 }
