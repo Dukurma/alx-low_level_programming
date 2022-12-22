6854f8dad937610bc2138155260ab6a570fb5534
Dukurma <barryyabilsu@gmail.com>
2022-12-22 12:42:43 -0800

    bettified

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,7 +14,7 @@ int main(void)
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to code anything else
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
