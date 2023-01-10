d6fc40596757233cb49ea419b58a61e9698fd1a9
Dukurma <barryyabilsu@gmail.com>
2023-01-10 11:04:47 -0800

    two

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,36 +3,42 @@
 
 /**
  * *str_concat - main function.
- * @s1: input one to concat
- * @s2: input two to concat
+ * @s1: Primer string.
+ * @s2: Segundo string.
+ *
+ *  Description: Escriba una función que devuelva un puntero a
+ *  un espacio recién asignado en la memoria, que contiene
+ *  una copia de la cadena dada como parámetro.
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	int size; /* Tamaño del string*/
+	int size2; /* Tamañano del segundo string */
+	int i, j; /* Iteradores de bucle */
+	char *array; /* Salida */
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
+	if (!s1)
+		s1 = ""; /* Devuelve un string vacio */
+	if (!s2)
 		s2 = "";
+	for (size = 0; s1[size] != '\0'; size++) /* leng del string */
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++) /* leng string */
+		;
+	array = malloc(((size) + (size2 + 1)) * sizeof(char)); /* New memory alloc */
 
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
-		return (NULL);
-
-	for (index = 0; s1[index]; index++)
-
-		concat_str[concat_index++] = s1[index];
-
-	for (index = 0; s2[index]; index++)
-
-		concat_str[concat_index++] = s2[index];
-
-	return (concat_str);
+	if (!array)
+	{
+		return (NULL); /* Devuelve NULLL si malloc falla */
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = s1[i]; /* Copiamos string en base a string dado por main */
+	}
+	for (j = 0; j < (size2 + 1); j++)
+	{
+		array[i + j] = s2[j]; /* Copiamos string en base a string dado por main */
+	}
+	return (array); /* Devolvemos string nuevo */
 }
