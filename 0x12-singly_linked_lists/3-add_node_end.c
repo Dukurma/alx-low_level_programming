7abbf0ed13f57d98aca0e8d3de5fe0785e672cfa
Dukurma <barryyabilsu@gmail.com>
2023-01-29 06:47:39 -0800

    correct update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,47 +1,44 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add_node_end - a function that adds a new node
+ * at the end of a list_t list.
+ * @head: input
+ * @str: input
+ * Return: 0
  */
-int main(void)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *head;
+	int i, count = 0;
+	list_t *new;
+	list_t *temp;
 
-	head = NULL;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-	add_node_end(&head, "Anne");
-	add_node_end(&head, "Colton");
-	add_node_end(&head, "Corbin");
-	add_node_end(&head, "Daniel");
-	add_node_end(&head, "Danton");
-	add_node_end(&head, "David");
-	add_node_end(&head, "Gary");
-	add_node_end(&head, "Holden");
-	add_node_end(&head, "Ian");
-	add_node_end(&head, "Ian");
-	add_node_end(&head, "Jay");
-	add_node_end(&head, "Jennie");
-	add_node_end(&head, "Jimmy");
-	add_node_end(&head, "Justin");
-	add_node_end(&head, "Kalson");
-	add_node_end(&head, "Kina");
-	add_node_end(&head, "Matthew");
-	add_node_end(&head, "Max");
-	add_node_end(&head, "Michael");
-	add_node_end(&head, "Ntuj");
-	add_node_end(&head, "Philip");
-	add_node_end(&head, "Richard");
-	add_node_end(&head, "Samantha");
-	add_node_end(&head, "Stuart");
-	add_node_end(&head, "Swati");
-	add_node_end(&head, "Timothy");
-	add_node_end(&head, "Victor");
-	add_node_end(&head, "Walton");
-	print_list(head);
-	return (0);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	new->len = i;
+	new->str = strdup(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+
+	temp->next = new;
+
+	return (new);
 }
+
