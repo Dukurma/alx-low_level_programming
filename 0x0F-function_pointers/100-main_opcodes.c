111aa19f01a3a342a7a2e10a70cbee55f8b88926
Dukurma <barryyabilsu@gmail.com>
2023-03-16 13:22:39 -0700

    task adv…

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include "function_pointers.h"
-
-
 
 /**
- * Intresting note: functions are a set of instruction and
- * function pointers are pointers to the begning of this instruction
- * so if we could get the first instrucions adress inmemory
- * we can see what it is doing by printing the adresses
- */
-
-/**
- * main - print opcodes of its own main function
+ * main - prints its own opcodes
  * @argc: number of arguments
- * @argv: vector of arguments
- * Return: 0 if no errors
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	register int i, num;
-	char *ptr = (char *)main;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	num = atoi(argv[1]);
-	if (num < 0)
-		printf("Error\n"), exit(2);
-	for (i = 0; i < num - 1; i++)
-		printf("%02hhx ", ptr[i]);
-	printf("%02hhx\n", ptr[i]);
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }
