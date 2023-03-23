f5979802e5a6271fea80afdc83c836f9e10138b7
Dukurma <barryyabilsu@gmail.com>
2023-03-23 16:03:00 -0700

    pot_of_gold

--- 0x18-dynamic_libraries/potofgold.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>                            #include <unistd.h>
-                                
-int printf(const char *format, ...)                  {
-/* we use write for printing the text
-*/             
-write(1, "9 8 10 24 75 - 9\n", 17);                  write(1, "Congratulations, you win the Jackpot!\n", 38);
-return(EXIT_SUCCESS)
-}
